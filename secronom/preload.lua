a83536991dcc4933b49fe6e8076cbfb99a71ea30
Axema Vales <41373960+Axema@users.noreply.github.com>
2019-03-11 16:38:46 +0800

    LUA functions for flesh cavern mechanics

--- secronom/preload.lua
+++ secronom/preload.lua
@@ -1,5 +1,3 @@
--- I AM BUSY WITH LUA. Yes, that's the reason of my inactivity at the forums. But still, I lack knowledge about it, so expect that my works has limitations... HELP!
-
 require("data/mods/secronom/LUA/functions")
 require("data/mods/secronom/LUA/-Essentials/secro_attacks")
 
@@ -9,9 +7,45 @@ mods["secronom"] = MOD
 
 -- Mod
 
+MOD.on_turn_passed = function()
+	
+	-- Prevent in-game performance drop by adding more conditions before launch. You will experience a sudden lag in this process, so don't panic.
+	if player:has_effect(efftype_id("flesh_breakdown")) == true and -- Check player if they killed the heart
+	map:tername(player:pos()) == "fleshy floor" then -- and if the player is stepping on a flesh floor
+	for delta_x = -100, 100 do
+		for delta_y = -100, 100 do -- Whole overmap special gets changed by this. Player must step on a flesh floor, not downward holes.
+			local point = player:pos()
+			point.x = point.x + delta_x
+			point.y = point.y + delta_y
+			if map:tername(point) == "fleshy wall" then 
+				map:ter_set(point, ter_str_id("t_rock")) -- Flesh area becomes a normal cavern
+				end
+			end
+		end
+	end
+	
+	if player:has_effect(efftype_id("flesh_breakdown")) == true and 
+	map:tername(player:pos()) == "fleshy floor" then
+	for delta_x = -100, 100 do
+		for delta_y = -100, 100 do
+			local point = player:pos()
+			point.x = point.x + delta_x
+			point.y = point.y + delta_y
+			if map:tername(point) == "fleshy floor" then
+				map:ter_set(point, ter_str_id("t_rock_floor"))
+				end
+			end
+		end
+	end
+	
+	if g:is_in_sunlight(player:pos()) == true then
+		player:remove_effect(efftype_id("flesh_breakdown"))
+	end
+	
+end
+
 MOD.on_minute_passed = function()
 	
-	-- Constant debuffs entering the flesh cavern. Lesser or no damage applied to player whether they got environmental resistance on a randomly targeted body part.
 	if map:tername(player:pos()) == "fleshy floor" then
 		local bodypart = player:get_random_body_part()
 		local bodypartdmgmod = math.random(3)
@@ -26,10 +60,11 @@ MOD.on_minute_passed = function()
 			local msg = math.random(32)
 			player:apply_damage(player, bodypart, bodypartdmg) -- Source of damage? Can't say null cuz codes
 			if msg == 31 then
-				game.add_msg("<color_red>You felt consumed...</color>")
+				game.add_msg("<color_red>You felt consumed...</color>") -- Give player a notice to get out if above conditions were met.
 			elseif msg == 32 then
 				game.add_msg("<color_red>The burden on your feet intensifies...</color>")
 			end
 		end
-	end
+	end -- An unprepared player may not survive 1 or 2 z-levels
+
 end
